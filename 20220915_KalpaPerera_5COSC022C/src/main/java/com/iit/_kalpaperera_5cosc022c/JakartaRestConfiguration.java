4ba6f602039d2a6b8d513c4a6328625ff1daf6ec
kalpa Perera <klperera660@gmail.com>
2025-04-20 23:15:28 +0530

    basic project structure

---
+++ 20220915_KalpaPerera_5COSC022C/src/main/java/com/iit/_kalpaperera_5cosc022c/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.iit._kalpaperera_5cosc022c;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
