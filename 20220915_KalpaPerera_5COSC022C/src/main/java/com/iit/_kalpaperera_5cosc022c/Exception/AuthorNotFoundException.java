b31b20fa40f8548be0a3863072ef90831f4ca28c
kalpa Perera <klperera660@gmail.com>
2025-04-23 03:46:23 +0530

    Create AuthorNotFoundException.java

---
+++ 20220915_KalpaPerera_5COSC022C/src/main/java/com/iit/_kalpaperera_5cosc022c/Exception/AuthorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.iit._kalpaperera_5cosc022c.Exception;
+
+public class AuthorNotFoundException extends RuntimeException {
+    public AuthorNotFoundException(String message) {
+        super(message);
+    }
+}
